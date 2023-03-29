416cd27fe06f64003a244a79ed44d6848c5a9d89
Aleks <jooksja1@gmail.com>
2023-03-29 21:48:24 +0300

    build

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
- <HashRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
- </HashRouter>
-
+    <React.StrictMode>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </React.StrictMode>
 );
 
 
